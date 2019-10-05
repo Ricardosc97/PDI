83635985e3ed3336c4b82534ee8117967313a0cf
Ricardosc97 <15-11370@usb.ve>
2019-10-05 13:24:06 -0400

    fixed an image

--- Test.py
+++ Test.py
@@ -2,9 +2,19 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-img = cv.imread('rose.jpg')
-dst = cv.resize(img,None,fx=2, fy=2, interpolation = cv.INTER_CUBIC)
+img1 = cv.imread('CITE.jpg')
+img2 = cv.resize(img1, None, fx = 1, fy = .9, interpolation = cv.INTER_CUBIC)
+cv.imshow("input",img1)
+cv.imshow("output", img2)
+cv.waitKey(0)
+cv.destroyAllWindows()
+cv.imwrite('CITE2.jpg',img2)
 
-plt.subplot(121),plt.imshow(img),plt.title('Input')
-plt.subplot(122),plt.imshow(dst),plt.title('Output')
-plt.show()
+#plt.subplot(121)
+#plt.title('input')
+#plt.imshow(img1)
+#plt.subplot(122)
+#plt.title('output')
+#plt.imshow(img2)
+#plt.show()
+
